44913764caed26a8d638a98b7a420e33bccf1458
letquit <1446444589@qq.com>
2026-02-28 12:41:43 +0800

    PlayerMovement

---
+++ Assets/Scripts/Components/CharacterAuthoring.cs
@@ -0,0 +1,67 @@
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Physics;
+using Unity.Transforms;
+using UnityEngine;
+
+public struct InitializeCharacterFlag : IComponentData, IEnableableComponent
+{
+    
+}
+
+public struct CharacterMoveDirection : IComponentData
+{
+    public float2 Value;
+}
+
+public struct CharacterMoveSpeed : IComponentData
+{
+    public float Value;
+}
+
+public class CharacterAuthoring : MonoBehaviour
+{
+    public float MoveSpeed;
+    
+    private class Baker : Baker<CharacterAuthoring>
+    {
+        public override void Bake(CharacterAuthoring authoring)
+        {
+            var entity = GetEntity(TransformUsageFlags.Dynamic);
+            AddComponent<InitializeCharacterFlag>(entity);
+            AddComponent<CharacterMoveDirection>(entity);
+            AddComponent(entity, new CharacterMoveSpeed
+            {
+                Value = authoring.MoveSpeed,
+            });
+        }
+    }
+}
+
+[UpdateInGroup(typeof(InitializationSystemGroup))]
+public partial struct CharacterInitializationSystem : ISystem
+{
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        foreach (var (mass, shouldInitialize) in SystemAPI.Query<RefRW<PhysicsMass>, EnabledRefRW<InitializeCharacterFlag>>())
+        {
+            mass.ValueRW.InverseInertia = float3.zero;
+            shouldInitialize.ValueRW = false;
+        }
+    }
+}
+
+public partial struct CharacterMoveSystem : ISystem
+{
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        foreach (var (velocity, direction, speed) in SystemAPI.Query<RefRW<PhysicsVelocity>, CharacterMoveDirection, CharacterMoveSpeed>())
+        {
+            var moveStep2d = direction.Value * speed.Value;
+            velocity.ValueRW.Linear = new float3(moveStep2d, 0f);
+        }
+    }
+}
